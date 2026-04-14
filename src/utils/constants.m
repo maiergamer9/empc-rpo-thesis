b81c02d9e3db1c67d00bd70ee07ed470efab3b55
maiergamer9 <maier_nils@web.de>
2026-04-14 11:25:21 +0900

    Add Mission Parameters (ISS as reference) to constants.m

--- src/utils/constants.m
+++ src/utils/constants.m
@@ -8,3 +8,9 @@ mu = 3.986004418e14;            % gravitational parameter mu = G*M [m^3/s^2]
 R_earth = 6371000;              % Earth Radius [m]
 omega_earth = 7.2921150e-5;     % Earth Rotation Rate [rad/s]
 
+%% Mission Parameters (ISS values, for start)
+a = 6771000;                    % Semi-major axis [m] (400km altitude of ISS)
+e = 0;                          % eccentricity [-] (circular orbit, assumption for HCW)
+i = deg2rad(51.6);              % Inclination [rad] (ISS)
+
+
