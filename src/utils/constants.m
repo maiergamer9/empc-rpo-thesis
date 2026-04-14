dd717b957f8f6f9d8a04e3f2727b21e0df646d33
maiergamer9 <maier_nils@web.de>
2026-04-14 11:21:00 +0900

    Add Earth parameters to constants.m

---
+++ src/utils/constants.m
@@ -0,0 +1,10 @@
+% constants.m
+% This file contains all physical and mission relevant constants
+% For consistency reasons, alls values are in SI units unless otherwise
+% noted
+
+%% Earth Parameters
+mu = 3.986004418e14;            % gravitational parameter mu = G*M [m^3/s^2]
+R_earth = 6371000;              % Earth Radius [m]
+omega_earth = 7.2921150e-5;     % Earth Rotation Rate [rad/s]
+
